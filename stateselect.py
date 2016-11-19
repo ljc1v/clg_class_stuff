e487fa69aacaabb0fde3cd7b016c9894705229ad
ljc1v <ljc1@cornell.edu>
2016-11-19 12:50:18 -0500

    Add files via upload

---
+++ stateselect.py
@@ -0,0 +1,45 @@
+#read in CVC file,  print out HTML file
+
+with open("states.csv","r") as inputfile:
+	states= inputfile.read().split("\n")
+
+#print intput variable
+print states
+
+#print tranformation
+for index, state in enumerate(states):
+	print "states[{0}] is {1}".format(index, states[index])
+	states[index] = state.split(",")
+	print "states[{0}] is {1} ".format(index, states[index])
+
+#print nested loops
+print states
+
+#print select list
+
+print "<select name='state'>"
+for index, state in enumerate(states):
+	print '<option value ="{0}"">{1}</option>'.format(state[0],state[1])
+print '</select>'
+
+
+# now print to a file
+
+with open ("states.html", "w") as new_file:
+	new_file.write("<select name='state'>\n")
+	for index, state in enumerate(states):
+ 	#print "index {0}, state {1}".format(index,state)
+		new_file.write ('<option value ="{0}"">{1}</option>\n'.format(state[0],state[1]))
+	new_file.write('</select>')
+
+
+
+	
+
+
+
+
+
+
+
+
