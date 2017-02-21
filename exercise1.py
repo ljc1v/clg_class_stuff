606e6c0d6f0b1b26be6df72a2b105335a08adf90
ljc1v <ljc1@cornell.edu>
2017-02-21 12:57:48 -0500

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,28 @@
+
+
+
+# email="laurie@cornell.edu"
+# position = email.find("@")
+# print position
+# userName = email[0:position]
+# print userName
+# len_username = len(userName)
+# print len_username
+# print "UserName is {0} it's length is {1}".format(userName,len_username)
+
+# saying = "     THe fox jumped over the log and bumped his head.         "
+# print saying
+# print saying.strip()
+
+# jump ='low'
+# if jump == 'high':
+# 	print "high"
+# 	print "low"
+# 	print "do a danced"
+
+# print "done with that"
+
+dogs = 2
+cats = 8
+if (dogs == 2)  and ((cats*2) > dogs):
+	print "cats rule"
